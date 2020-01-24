4605bed2623cb561dd124094e0a722a8a1e8201c
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-01-23 19:08:02 -0600

    Se agregó el hadouken y la pantalla de los controles al inicio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 const $canvas = document.querySelector('canvas')
 const ctx = $canvas.getContext('2d')
+
+const one_player = document.querySelector('#one_player')
+const two_players = document.querySelector('#two_players')
+const demo = document.querySelector('#demo')
+
+const sound = new Audio()
+const sound_hit = new Audio()
+const sound_shock = new Audio()
+const ko_sound = new Audio()
+const deadth_sound = new Audio()
+const hadouken = new Audio()
+
+const ko_image = new Image()
+const you_win = new Image()
+
+const powers = []
 let interval
 let frames = 0
 let game
@@ -8,27 +24,39 @@ let f2
 let bg 
 let timeout = 0
 
-const sound = new Audio()
+
 sound.src = "sound/ryu.mp3"
-const sound_hit = new Audio()
 sound_hit.src = "sound/hit.wav"
-const sound_shock = new Audio()
 sound_shock.src = "sound/shock.wav"
-
-const ko_sound = new Audio()
 ko_sound.src = "sound/ko_sound.mp3"
-const deadth_sound = new Audio()
 deadth_sound.src = "sound/deadth_sound.mp3"
+hadouken.src = "sound/hadouken.mp3"
 
-const ko_image = new Image()
+
 ko_image.src = "images/ko.png"
-
-const you_win = new Image()
 you_win.src = "images/you_win.png"
 
 const characters = {
     ryu: "images/ryu_moves.png",
     guile: "images/guile_moves.png",
+}
+
+class Habilities {
+    constructor(x, y, direction){
+        this.x = x
+        this.y = y
+        this.sx = -40
+        this.direction = !direction
+        this.img = new Image()
+        this.img.src = (this.direction) ? characters.ryu : characters.guile
+        this.img.onload = ()=> this.Draw()
+    }
+    Draw(){
+        this.x = this.x +15 
+        this.sx += 40
+        if( this.sx > 80 ) this.sx = 0
+        ctx.drawImage(this.img, this.sx, 400, 40,40,this.x, this.y +50,40, 40)
+    }
 }
 
 class Fighter {
@@ -42,15 +70,12 @@ class Fighter {
         this.width = 100
         this.intervalDesition
         this.auto_machine = false
+        this.sendHability = false
         
         this.direction = player2
         this.img_source = new Image()
         this.img_source.src = character
-        this.img_source.onload = ()=>{
-            
-            
-            this.Draw()
-        }
+        this.img_source.onload = () => this.Draw()
         this.isKO = false
         this.isMoving = true
         this.isJumping = false
@@ -71,7 +96,6 @@ class Fighter {
             ctx.scale(1,1)
         }
         
-        
         ctx.drawImage(this.img_source, 
             this.move.start+this.move.x,
             this.move.y,
@@ -85,6 +109,7 @@ class Fighter {
 
     idle() {
         this.attack = false
+        this.sendHability = false
         this.width = 100
         this.height = 180
         this.move = {
@@ -105,19 +130,17 @@ class Fighter {
     }
     back(){
         this.move.start = 200
-            this.move.offset = 50
-            this.move.end = 250
+        this.move.offset = 50
+        this.move.end = 250
         this.x -= 10
     }
     down(){
-        
         this.move.start = 1165
         this.move.offset = 35
         this.move.end = 35
     }
     jump(){
         this.isJumping = true
-
         this.move.start = 450
         this.move.offset = 50
         this.move.end = 300
@@ -165,12 +188,16 @@ class Fighter {
     }
 
     hability(){
-        console.log("Hability")
-        this.width = this.width +50
-        this.move.y = 300
-        this.move.start = 400
-        this.move.offset = 100
-        this.move.end = 100
+        if( !this.direction ) hadouken.play()
+        if( !this.sendHability ){
+            this.sendHability = true
+            this.width = this.width + 50
+            powers.push(new Habilities(this.x + this.width -60 , this.y, this.direction))
+            this.move.y = 300
+            this.move.start = 400
+            this.move.offset = 100
+            this.move.end = 100
+        }
     }
 
     ko() {
@@ -182,7 +209,6 @@ class Fighter {
         this.move.start = 0
         this.move.offset = 90
         this.move.end = 360
-        
     }
 }
 
@@ -192,6 +218,7 @@ class Game{
         this.gameover_showed = false
         this.player1 = pl1
         this.player2 = pl2
+        this.delayToTakeDesition = 30
         this.checkHealth()
         bg = new Image()
         bg.src = "images/ryu_stage.jpg"
@@ -207,18 +234,12 @@ class Game{
         ctx.drawImage(bg, 0,0, $canvas.width, $canvas.height)
     }
     checkKnock() {
-        
         if(this.player1.x + 100 > -this.player2.x-100) {
-            if( this.player1.attack ) {
-                this.player2.hit(this.player1.power) 
-            }
-            if( this.player2.attack ) {
-                this.player1.hit(this.player2.power)
-            }
-            // (this.player2.attack) ? this.player1.hit() : null
+            ( this.player1.attack ) ? this.player2.hit(this.player1.power) : null
+            ( this.player2.attack ) ? this.player1.hit(this.player2.power) : null
+
             this.player1.x -= 20
             this.player2.x -= 20
-            
         }
         if( -this.player2.x-this.player1.width >= $canvas.width -this.player2.width){
             this.player2.x = -$canvas.width +this.player2.width
@@ -245,7 +266,6 @@ class Game{
         ctx.fillStyle = "yellow"
         ctx.fillRect(mid_width -(player1_health+20), 20, player1_health, 30)
         ctx.fillRect(mid_width +20, 20, player2_health, 30)
-
     }
 
     DrawGameOver(){
@@ -277,74 +297,82 @@ class Game{
         }
     }
 
-
-
-    isGameOver(){
-        return ((this.player1.isKO || this.player2.isKO))
-    }
+    isGameOver(){ return ((this.player1.isKO || this.player2.isKO)) }
 
     SpaceBetweenBoth(){
         const space_between = -this.player2.x -this.player1.x 
         return (space_between > 250)
     }
 
+    ejectDesition(player, action){
+        switch( action ){
+            case 0: player.idle(); break
+            case 1: player.punch(); break
+            case 2: player.kick(); break
+            case 3: player.back(); break
+            default : player.idle(); break
+        }
+    }
+
+    CheckCollition(){
+        if( powers.length ){
+            powers.forEach( (power, i) => {
+                const power_guile = power.img.src.includes( characters.guile )
+                for( let j = i+1; j < powers.length ; j++){
+                    let cur_power
+                    let other_power
+
+                    if( power_guile ){
+                        other_power = (power.x < 0) ? -power.x : power.x
+                        cur_power = (powers[j].x < 0) ? -powers[j].x : powers[j].x +40
+
+
+                    }else{
+                        cur_power = (power.x < 0) ? -power.x : power.x +90
+                        other_power = (powers[j].x < 0) ? -powers[j].x : powers[j].x
+                    }
+                    if( cur_power > other_power ){
+                        powers.splice(j,1)
+                        powers.splice(i,1)
+                    }
+                    
+
+                }
+                
+                if( power.img.src.includes(characters.ryu)){
+                    
+                    if(power.x + 40 > -this.player2.x-100) {
+                        this.player2.hit(10)
+                        powers.splice(i,1)
+                    } 
+                }
+                if( power.img.src.includes(characters.guile) ){
+                    if(-power.x < this.player1.x+120 ) {
+                        this.player1.hit(10)
+                        powers.splice(i,1)
+                    } 
+                }
+            })
+        }
+    }
+
     takeDesition() {
         if( this.isGameOver() ) return
         if( this.player2.auto_machine ){
-            if( this.SpaceBetweenBoth() ){
-                 this.player2.go()
-            }else{
-                const action = Math.floor(Math.random()*30)
-                switch( action ){
-                    case 0:
-                          this.player2.idle()
-                          break
-                     case 1:
-                          this.player2.punch()
-                          break
-                     case 2:
-                          this.player2.kick()
-                          break
-                     case 3:
-                          this.player2.back()
-                          break
-                    default :
-                         this.player2.idle()
-                         break
-                }
-                
-            }
+            ( this.SpaceBetweenBoth() )
+                ? this.player2.go()
+                : this.ejectDesition(this.player2, Math.floor(Math.random()* this.delayToTakeDesition ) )
         }
 
         if( this.player1.auto_machine ){
-            if( this.SpaceBetweenBoth() ){
-                this.player1.go()
-            }else{
-                const action2 = Math.floor(Math.random()*30)
-                switch( action2 ){
-                    case 0:
-                          this.player1.idle()
-                          break
-                     case 1:
-                          this.player1.punch()
-                          break
-                     case 2:
-                          this.player1.kick()
-                          break
-                     case 3:
-                          this.player1.back()
-                          break
-                    default :
-                     this.player1.idle()
-                     break
-                }
-                
-            }
+            ( this.SpaceBetweenBoth() )
+                ? this.player1.go()
+                : this.ejectDesition( this.player1, Math.floor(Math.random()* this.delayToTakeDesition ) )
         }
-
     }
-
 }
+
+
 function checkingJumping(){
     if( game.player1.isJumping ) {
         if(game.player1.y > 100){
@@ -371,13 +399,14 @@ function update(){
     ctx.clearRect(0,0, $canvas.width, $canvas.height)
     game.showStage()
     game.DrawGameOver()
+    powers.forEach(power=>power.Draw())
+    game.CheckCollition()
     game.takeDesition()
     game.checkHealth()
     checkingJumping()
     game.checkKnock()
     game.Draw()
     if( game.isGameOver() && frames >= timeout ) clearInterval(interval)
-
 }
 
 function show_menu(){
@@ -391,31 +420,18 @@ function hide_menu(){
     document.querySelector('.splash').style.visibility = 'hidden'
 }
 
-function start_game(){
-    // game = new Game(
-    //     new Fighter(characters.ryu, false),
-    //     new Fighter(characters.guile, true))
-
-    // // game.player1.auto_machine = true
-    // // game.player2.auto_machine = true
-    // interval = setInterval(update, 100)
-}
-const a = new Audio()
-
-window.onload = ()=>{
-    $canvas.style.visibility = 'hidden'
-}
 
 
+window.onload = ()=>{ $canvas.style.visibility = 'hidden' }
 window.onkeydown = ({keyCode})=>{
-    
 
     game.player1.keys[keyCode] = true
     game.player2.keys[keyCode] = true
 
     if( keyCode === 81 ) return show_menu()
 
-    if( game.player1.keys[17] && game.player1.keys[39]) game.player1.punch()
+    if( game.player1.keys[17] && game.player1.keys[39] && game.player1.keys[40] ) game.player1.hability()
+    else if( game.player1.keys[17] && game.player1.keys[39]) game.player1.punch()
     else if(game.player1.keys[17] && game.player1.keys[37]) game.player1.punch()
     else if(game.player1.keys[16] && game.player1.keys[39]) game.player1.kick()
     else if(game.player1.keys[16] && game.player1.keys[37]) game.player1.kick()
@@ -426,7 +442,8 @@ window.onkeydown = ({keyCode})=>{
         if( game.player1.keys[16] && !game.player1.isJumping )  game.player1.kick()
     }
 
-    if( game.player2.keys[65] && game.player2.keys[74]) game.player2.punch()
+    if(game.player2.keys[75] && game.player2.keys[65] && game.player2.keys[74]) game.player2.hability()
+    else if( game.player2.keys[65] && game.player2.keys[74]) game.player2.punch()
     else if( game.player2.keys[65] && game.player2.keys[76]) game.player2.punch()
     else if(game.player2.keys[83] && game.player2.keys[74]) game.player2.kick()
     else if(game.player2.keys[83] && game.player2.keys[76]) game.player2.kick()
@@ -440,25 +457,19 @@ window.onkeydown = ({keyCode})=>{
 }
 
 window.onkeyup = ({keyCode})=>{
-    // console.log(keyCode)
     game.player1.keys[keyCode] = false
     game.player2.keys[keyCode] = false
+
     keyUp1 = (keyCode == 39 || keyCode == 37 || keyCode == 40 || keyCode == 17 || keyCode == 16)
-    if(keyUp1){
-        game.player1.idle()
-
-    }
     keyUp2 = (keyCode == 74 || keyCode == 76 || keyCode == 65 || keyCode == 83)
-    if(keyUp2){
-        game.player2.idle()
 
-    }
+    if(keyUp1){ game.player1.idle() }
+    if(keyUp2){ game.player2.idle() }
+
     return false
 }
 
-const one_player = document.querySelector('#one_player')
-const two_players = document.querySelector('#two_players')
-const demo = document.querySelector('#demo')
+
 
 one_player.onclick = () =>{
     hide_menu()
@@ -484,8 +495,10 @@ demo.onclick = ()=>{
         new Fighter(characters.ryu, false),
         new Fighter(characters.guile, true))
 
-    game.player1.health = 30
-    game.player2.health = 30
+    game.delayToTakeDesition = 15
+
+    game.player1.health = 50
+    game.player2.health = 50
 
     game.player1.auto_machine = true
     game.player2.auto_machine = true
